ed3024ed739bbb063b40e21c2e69837bd9dc5f86
oarrivi <oarrivi@hotmail.com>
2015-06-25 09:37:00 +0200

    Add support to report progress and cancellation

--- ArriviSoft.AsyncTasksApp/Model/ITaskService.cs
+++ ArriviSoft.AsyncTasksApp/Model/ITaskService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ArriviSoft.AsyncTasksApp.Model
@@ -13,41 +14,13 @@ namespace ArriviSoft.AsyncTasksApp.Model
     public interface ITaskService
     {
         /// <summary>
-        /// Occurs when [status changed].
-        /// </summary>
-        event TaskProgressChangedEventHander StatusChanged;
-
-        /// <summary>
-        /// Occurs when [task finished].
-        /// </summary>
-        event AsyncTaskFinishedEventHandler TaskFinished;
-
-        /// <summary>
         /// Gets the name of the task.
         /// </summary>
         string Name { get; }
 
         /// <summary>
-        /// Gets the progress of the task. This values range is from 0 to 1.
-        /// </summary>
-        double Progress { get; }
-
-        /// <summary>
-        /// Gets the result of the execution: The time taken to execute the slow task.
-        /// </summary>
-        /// <value>
-        /// The time elapsed.
-        /// </value>
-        TimeSpan Result { get; }
-
-        /// <summary>
-        /// Request the cancelation of the task.
-        /// </summary>
-        void CancelAsync();
-
-        /// <summary>
         /// Starts the task in asynchronous mode.
         /// </summary>
-        Task<TaskResult> StartAsync(IProgress<TaskProgressChangedEventArgs> progress);
+        Task<TaskResult> StartAsync(IProgress<double> progress, CancellationToken cancellationToken);
     }
 }
--- ArriviSoft.AsyncTasksApp/Model/WorkItemService.cs
+++ ArriviSoft.AsyncTasksApp/Model/WorkItemService.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ArriviSoft.AsyncTasksApp.Model
@@ -13,10 +14,6 @@ namespace ArriviSoft.AsyncTasksApp.Model
     /// </summary>
     public class WorkItemService : ITaskService
     {
-        private object userData = null;
-
-        public event TaskProgressChangedEventHander StatusChanged;
-        public event AsyncTaskFinishedEventHandler TaskFinished;
 
         public WorkItemService(string name)
         {
@@ -24,29 +21,30 @@ namespace ArriviSoft.AsyncTasksApp.Model
         }
 
         public string Name { get; private set; }
-        
-        public double Progress { get; private set; }
 
-        public TimeSpan Result { get; private set; }
 
-        public void CancelAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<TaskResult> StartAsync(IProgress<TaskProgressChangedEventArgs> progress)
+        /// <summary>
+        /// Starts the task in asynchronous mode.
+        /// </summary>
+        /// <param name="progress"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task<TaskResult> StartAsync(IProgress<double> progress, CancellationToken cancellationToken)
         {
 
             var task = Task.Run<TimeSpan>(async () =>
                 {
+                    Random rand = new Random();
+                    int delay = Convert.ToInt32(rand.NextDouble() * 150);
+
                     Stopwatch sw = Stopwatch.StartNew();
 
                     for (int i = 0; i <= 100; i++)
                     {
-                        await Task.Delay(100);
-                        progress.Report(new TaskProgressChangedEventArgs(Convert.ToDouble(i) / 100.0));
+                        cancellationToken.ThrowIfCancellationRequested();
+                        await Task.Delay(delay);
+                        progress.Report(Convert.ToDouble(i) / 100.0);
                     }
-
 
                     sw.Stop();
 
@@ -56,30 +54,7 @@ namespace ArriviSoft.AsyncTasksApp.Model
             // Await here until task finishes...
             TimeSpan ts = await task;
 
-            var result = new TaskResult(ts);
-            
-            // Note: It isn't necessary to raise events
-
-            return result;
+            return new TaskResult(ts);
         }
-
-        private void OnTaskFinished(Exception error, bool cancelled, TimeSpan result)
-        {
-            var handler = this.TaskFinished;
-            if (handler != null)
-            {
-                handler(this, new AsyncTaskFinishedEventArgs(error, cancelled, result, this.userData));
-            }
-        }
-
-        private void OnStatusChanged(double progress)
-        {
-            var handler = this.StatusChanged;
-            if (handler != null)
-            {
-                handler(this, new TaskProgressChangedEventArgs(progress));
-            }
-        }
-
     }
 }
